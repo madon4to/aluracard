3660577678e01705a5fe81f56daee66e70098e78
madon4to <marozaa1301@gmail.com>
2024-11-06 08:14:28 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'FILOSOFIA',
+    'De quem é a famosa frase “Penso, logo existo?',
+    'Descartes'
+)
+
+criaCartao(
+    'DESCOBRIMENTO',
+    'De onde é a invenção do chuveiro elétrico?',
+    'Brasil'
+)
+
+criaCartao(
+    'GEOGRAFIA',
+    'Quais o menor e o maior país do mundo?',
+    'Vaticano e Rússia' 
+)
+
+criaCartao(
+    'INGLÊS',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
